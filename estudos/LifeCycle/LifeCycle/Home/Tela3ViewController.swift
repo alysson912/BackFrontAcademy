bcc8fa2bf35c17931d25310d802d31e62f9d624b
Alysson M <almenezes912@gmail.com>
2022-10-06 20:37:04 -0300

    Fazendo navigation entre telas via codigo

---
+++ estudos/LifeCycle/LifeCycle/Home/Tela3ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Tela3ViewController.swift
+//  LifeCycle
+//
+//  Created by ALYSSON MENEZES on 06/10/22.
+//
+
+import UIKit
+
+class Tela3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func BotaoTela3(_ sender: Any) {
+        
+
+    }
+    
+}
