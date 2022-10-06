98c6e6bbc4d95cab38bc3a07913430450e4c6101
Alysson M <almenezes912@gmail.com>
2022-10-06 19:31:16 -0300

    aula Navigation

---
+++ conteudoAula/Navigation/Navigation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by ALYSSON MENEZES on 04/10/22.
+//
+//MARK: boa prtica  : desabilitar notification center, ex: notification  p/ exibir telas
+//MARK: o que é uma root view controller? -> tela raiz ou seja tela (home)
+//MARK: tela de baixo para cima: ex: de uso : tela login resolver tudo o que precisa resolver depois volta para tela anterior
+
+//MARK: comandos uteis
+//MARK: command + shift O mostra a tela que deseja
+//MARK: command + shift + J exibe o destino do file ( no lado esquedo do navigation)  usado em projetos grandes
+//MARK: toda situacao em que usamos o identificador, usa-se o opcional
+//MARK: acesso ao code snipet command + shift + L
+
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
