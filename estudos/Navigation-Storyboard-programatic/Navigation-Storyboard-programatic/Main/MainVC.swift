d5d66d58ad66bffd6b9651252c44074ec1a58f3e
Alysson M <almenezes912@gmail.com>
2022-10-11 19:52:31 -0300

    atualizando commit

---
+++ estudos/Navigation-Storyboard-programatic/Navigation-Storyboard-programatic/Main/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  Navigation-Storyboard-programatic
+//
+//  Created by ALYSSON MENEZES on 07/10/22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func tappedTela02(_ sender: UIButton) {
+      
+        let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+        
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+       // present(vc ?? UIViewController(), animated: true)
+        
+        print("tapped Button")
+    }
+    
+}
---
+++ estudos/Navigation-Storyboard-programatic/Navigation-Storyboard-programatic/Main/Tela02VC.swift
@@ -0,0 +1,21 @@
+//
+//  Tela02VC.swift
+//  Navigation-Storyboard-programatic
+//
+//  Created by ALYSSON MENEZES on 07/10/22.
+//
+
+import UIKit
+
+class Tela02VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
