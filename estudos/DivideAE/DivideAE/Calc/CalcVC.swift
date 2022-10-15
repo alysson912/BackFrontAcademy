6e673bbb3c8e7c7233420c8565b97815ad335d20
Alysson M <almenezes912@gmail.com>
2022-10-15 17:36:58 -0300

    App Finalizado (tudo funcionando)!

--- estudos/DivideAE/DivideAE/Calc/CalcVC.swift
+++ estudos/DivideAE/DivideAE/Calc/CalcVC.swift
@@ -19,7 +19,7 @@ class CalcVC: UIViewController {
     
     
     @IBOutlet weak var valorContaTotalTextField: UITextField!
-
+    
     @IBOutlet weak var calcContaButton: UIButton!
     
     override func viewDidLoad() {
@@ -27,10 +27,10 @@ class CalcVC: UIViewController {
         self.addElement()
         configTapGesture()
     }
-
+    
     func addElement(){
-     
         
+        qtdDivisaoLabel.text = String(Int(1))
         mainView.backgroundColor = .white
         mainView.layer.shadowOffset = CGSize(width: 2, height: 6)
         mainView.layer.masksToBounds=false
@@ -45,7 +45,7 @@ class CalcVC: UIViewController {
         valorContaTotalTextField.delegate = self
         
         gorjetaSwitch.selectedSegmentIndex
-       
+        
         calcContaButton.setTitle("calc", for: .normal)
         calcContaButton.backgroundColor = .systemBlue
         calcContaButton.setTitleColor(.white, for: .normal)
@@ -53,78 +53,92 @@ class CalcVC: UIViewController {
         
         qtdDivisao.autorepeat = true
         qtdDivisao.maximumValue = 10
-        qtdDivisao.minimumValue = 0
+        qtdDivisao.minimumValue = 1
         
-  
-    }
-
-    @IBAction func tappedCalcButton(_ sender: Any) {
-        print("botao apertado")
+        
     }
     
-    @IBAction func gorjetaSelectSegmentControl(_ sender: Any) {
+    @IBAction func tappedCalcButton(_ sender: Any) {
+        print("botao apertado")
+        
+        if gorjetaSwitch.selectedSegmentIndex == 0 {
+            self.calcConta()
+        }else {
+            self.calcContaComGorjeta()
+        }
+        
         switch gorjetaSwitch.selectedSegmentIndex
-            {
-            case 0:
-                print("sem gorjeta")
-            case 1:
-                print("10% selecionado")
-            default:
-                break
-            }
+        {
+        case 0:
+            self.calcConta()
+        case 1:
+            print("10% selecionado")
+            self.calcContaComGorjeta()
+        default:
+            break
+        }
     }
-                                
+    
+    
+    
+    @IBAction func gorjetaSelectSegmentControl(_ sender: Any) {
+        
+    }
+    
     @IBAction func tappedQtdDivisaoLabel(_ sender: Any) {
-        print(qtdDivisao.value)
-        qtdDivisaoLabel.text = String(Int(qtdDivisao.value)) ?? ""
+        //print(qtdDivisao.value)
+        qtdDivisaoLabel.text = String(Int(qtdDivisao.value))
     }
     
     func calcConta(){
-        var valorConta = Double((valorContaTotalTextField.text?.replacingOccurrences(of: ",", with: ".")) ?? "0.0") ?? 00
-        var qtdDePessoas = Double(qtdDivisao.value)
+        let valorConta = Double((valorContaTotalTextField.text?.replacingOccurrences(of: ",", with: ".")) ?? "0.0") ?? 00
+        let qtdDePessoas = Double(qtdDivisao.value)
         
-      var calc = valorConta / qtdDePessoas
+        let calc = valorConta / qtdDePessoas
         
-        self.valorASerDivididoLabel.text? = String(calc) ?? ""
+        self.valorASerDivididoLabel.text? = String(calc)
+    }
+    
+    func calcContaComGorjeta(){
+        let valorConta = Double((valorContaTotalTextField.text?.replacingOccurrences(of: ",", with: ".")) ?? "0.0") ?? 00
+        let qtdDePessoas = Double(qtdDivisao.value)
+        
+        let calc = (valorConta / qtdDePessoas )
+        let gorjeta = calc + calc * 0.1
+        
+        self.valorASerDivididoLabel.text? = String(gorjeta)
     }
     
     private func configTapGesture(){
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CalcVC.handleTap))
-            view.addGestureRecognizer(tapGesture)
-        }
-        
-        @objc func handleTap(){
-            view.endEditing(true)
-        }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CalcVC.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(){
+        view.endEditing(true)
+    }
+    
 }
 
 extension CalcVC : UITextFieldDelegate{
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.layer.backgroundColor = UIColor.systemBlue.cgColor
-    }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
         if textField.hasText {
             textField.layer.borderColor = UIColor.lightGray.cgColor
         }else {
             textField.layer.borderColor = UIColor.red.cgColor
         }
         
-        if valorContaTotalTextField.text == ""  &&  qtdDivisao.value <= 0 {
-            
-            calcContaButton.isEnabled = false
-        }else {
-            calcContaButton.isEnabled = true
+            if valorContaTotalTextField.text == ""  &&  qtdDivisao.value <= 0 {
+                calcContaButton.isEnabled = false
+            }else {
+                calcContaButton.isEnabled = true
+            }
         }
-        
-        
-        calcConta()
-    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("baixando teclado")
         textField.resignFirstResponder()
-        
         return true
     }
 }
