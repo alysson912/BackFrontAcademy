d9d2f6b9e67ddf2849d63363d46425f86fa4643a
Alysson M <almenezes912@gmail.com>
2022-11-15 17:11:30 -0300

    commit inicial

---
+++ collectionView-BF-PLUS/collectionView-basico/collectionView-basico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionView-basico
+//
+//  Created by ALYSSON MENEZES on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
