52d92f562339440e51b26a3b39499bb7f7cb93da
Alysson M <almenezes912@gmail.com>
2022-11-17 15:40:39 -0300

    finalizando projeto

--- collectionView-BF-PLUS/praticando/tableVIewComCollectionView/tableVIewComCollectionView/ViewController.swift
+++ collectionView-BF-PLUS/praticando/tableVIewComCollectionView/tableVIewComCollectionView/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var homeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configHomeTableView()
     }
 
-
+    func configHomeTableView(){
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+        // register cell
+        homeTableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier)
+    }
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       return  250
+    }
+}
